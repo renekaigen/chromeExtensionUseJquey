14ba78572d617d47614d4622b44718292ac54194
Rene Soto Lira <rene.soto01@iest.edu.mx>
2015-11-30 11:31:32 -0600

    Ahora con jquery aplicado a los inputs internos del popup :)

--- popup.js
+++ popup.js
@@ -1,14 +1,27 @@
 var rene="sss";
 
+//trayendo el script de jquery
 chrome.tabs.executeScript(null, { file: "jquery.js" }, function() {
    // chrome.tabs.executeScript(null, { file: "alert.js" });
+
+
+   	$("#rb").val("asd");
+
+  
+   
 });
 
+function eto(){
+	//chrome.tabs.executeScript(null, { file: "jquery.js" }, function() {
+   	$("#rb").val("etooo");  
+//});
+}
+
+ 
+
 function hello() {
+	
 //rene=document.getElementById('clickme').value;
-
-
-
   chrome.tabs.executeScript({
     file: 'alert.js'
 
@@ -19,3 +32,4 @@ function hello() {
 
 
 document.getElementById('clickme').addEventListener('click', hello);
+document.getElementById('ejemplo').addEventListener('click', eto);
